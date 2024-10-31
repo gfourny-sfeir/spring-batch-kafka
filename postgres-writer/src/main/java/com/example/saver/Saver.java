80213541ff2ab8c72bbd19807f45334e416a0e16
Guillaume Fourny <fourny.g@sfeir.com>
2024-10-31 12:09:25 +0100

    Update Saver.java

--- postgres-writer/src/main/java/com/example/saver/Saver.java
+++ postgres-writer/src/main/java/com/example/saver/Saver.java
@@ -6,8 +6,11 @@ import org.springframework.jdbc.core.simple.JdbcClient;
 
 import com.example.model.Fourniture;
 
+import jakarta.annotation.Nonnull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import static org.apache.commons.lang3.ObjectUtils.allNotNull;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -15,7 +18,9 @@ public class Saver {
 
     private final JdbcClient jdbcClient;
 
-    public void save(Fourniture fourniture, String request, Map<String, ?> parameters) {
+    public void save(@Nonnull Fourniture fourniture, @Nonnull String request, @Nonnull Map<String, ?> parameters) {
+        allNotNull(fourniture, request, parameters);
+
         log.info("Saving fourniture {}", fourniture);
 
         jdbcClient.sql(request)
